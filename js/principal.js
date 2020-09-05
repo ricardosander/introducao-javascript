70dec065b0e32abf0fb5a54309dc06ca51faa12e
Ricardo Sander Lopes <ricardo.lopes@ifood.com>
2020-09-05 01:08:55 -0300

    Calculando IMC com validações.

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,25 @@
 var pagetTitle = document.querySelector('.titulo');
 pagetTitle.textContent = "Aparecida Nutricionista";
+
+const pacientes = document.querySelector('#tabela-pacientes');
+
+const peso = pacientes.querySelector('.info-peso').textContent;
+const altura = pacientes.querySelector('.info-altura').textContent;
+
+var imc = undefined;
+
+const recordMundiaDePeso = 597;
+if (peso <= 0 || peso > recordMundiaDePeso) {
+    imc = "Peso inválido.";
+}
+
+const recordMundialDeAltura = 2.74
+if (altura <= 0 || altura > recordMundialDeAltura) {
+    imc = "Altura inválida."
+}
+
+if (imc == undefined) {
+    var imc = peso / (altura * altura);
+}
+
+pacientes.querySelector(".info-imc").textContent = imc;
